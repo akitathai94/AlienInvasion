1166d810b64298014c22f57759cf52298d3ad0e6
Thai Truong <akita.thai94@gmail.com>
2021-01-19 19:31:44 -0800

    Update ship moving

--- ship.py
+++ ship.py
@@ -13,8 +13,24 @@ class Ship:
         self.rect = self.image.get_rect()
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
-    
+        # Movement flag 
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+        
+    def update(self):
+        """Update the ship 's position based on the movement flag."""
+        if self.moving_right:
+            self.rect.x += 1
+        elif self.moving_left:
+            self.rect.x -= 1
+        elif self.moving_up:
+            self.rect.y -= 1
+        elif self.moving_down:
+            self.rect.y += 1
+
     def blitme(self):
         """Draw the ship at its current location"""
         self.screen.blit(self.image, self.rect)
-        
+
