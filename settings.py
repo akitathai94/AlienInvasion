58d8d98cd221795aafff79392a5f242e2399ed95
Thai Truong <akita.thai94@gmail.com>
2021-01-21 13:14:29 -0800

    Update fleet, collision between aliens and bullets

--- settings.py
+++ settings.py
@@ -6,9 +6,14 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
-        self.ship_speed = 3
+        self.ship_speed = 5
         # Bullet settings
-        self.bullet_speed = 5
+        self.bullet_speed = 7
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
+        # Alien settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction of 1 represents right; -1 represent left
+        self.fleet_direction = 1
